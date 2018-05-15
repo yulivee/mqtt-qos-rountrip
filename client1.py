202648c5aa0b5f18ae55b1eb6e0f591adc946e11
Sandra <coding@voiding-warranties.de>
2018-05-15 13:03:45 +0200

    Replaced print statements with python logging library

--- client1.py
+++ client1.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python
 import paho.mqtt.client as mqtt
 import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# create a file handler
+handler = logging.FileHandler('client1.log')
+handler.setLevel(logging.INFO)
+
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(handler)
+
 
 def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
+    logger.info("message received - id:"+message.id+" - topic:"+message.topic+" - qos:"+message.qos+" - size:"+str(len(message.payload)))
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         client.connected_flag=True
-        print("OK - connected")
+	logger.info("Client connected to broker")
     else:
-        print("NOT OK - Return Code = ",rc)
+	logger.info("Client failed to connected to broker, Return Code = ", rc)
         client.loop_stop()
 
 def on_disconnect(client, userdata, rc):
-    print("OK - client disconnected")
+    logger.info("Client disconnected from broker")
 
 def on_publish(client,userdata,mid):             #create function for callback
-    print("data published, mid= ", mid)
+    logger.info("message sent - id:"+mid+" - topic:"+topic+" - qos:"+qos_level+" - size:"+str(len(message)))
 
 def on_log(client, userdata, level, buf):
-    print("log: ",buf)
+    #print("log: ",buf)
 
 def wait_for(client,msgType,period=0.25):
     if msgType=="SUBACK":
@@ -51,28 +64,18 @@ client.on_connect=on_connect
 client.on_disconnect=on_disconnect
 
 # Connection to Broker
-print("connecting to broker "+broker_address+":"+str(port))
+logger.info("connecting to broker "+broker_address+":"+str(port))
 client.connect(broker_address,port) #connect to broker
 client.loop_start() #start the loop
 
-#while not client.connected_flag:
-#    print("waiting for connect")
-#    time.sleep(2)
-
-print("Subscribing to topic "+answer_topic)
+logger.info("Subscribing to topic "+answer_topic)
 sub_rc = client.subscribe(answer_topic,qos_level)
-print("subscribe returned ",sub_rc)
+logger.info("subscribe returned ",sub_rc)
 wait_for(client, sub_rc)
  
-print("Publishing message to topic "+topic)
-# publish(topic, payload=None, qos=0, retain=False)
-
 i=10
 while (i!=0) :
-    print("publish message "+message+" on topic "+topic+", qos_level="+str(qos_level))
     pub_rc = client.publish(topic,message, qos_level, False)
-    print("publish returned ",pub_rc)
-    time.sleep(2) # wait
     i=i-1
 
 client.loop_stop() #stop the loop
--- client2.py
+++ client2.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python
 import paho.mqtt.client as mqtt
 import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# create a file handler
+handler = logging.FileHandler('client1.log')
+handler.setLevel(logging.INFO)
+
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(handler)
+
 
 def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
-    
-    publish_back(message.topic, message.payload, message.qos)
+    logger.info("message received - id:"+message.id+" - topic:"+message.topic+" - qos:"+message.qos+" - size:"+str(len(message.payload)))
+    publish_back(message.topic, message.payload, message.qos, message.id)
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         client.connected_flag=True
-        print("OK - connected")
+	logger.info("Client connected to broker")
     else:
-        print("NOT OK - Return Code = ",rc)
+	logger.info("Client failed to connected to broker, Return Code = ", rc)
         client.loop_stop()
 
 def on_disconnect(client, userdata, rc):
-    print("OK - client disconnected")
+    logger.info("Client disconnected from broker")
 
 def on_publish(client,userdata,mid):             #create function for callback
-    print("data published, mid= ", mid)
 
 def on_log(client, userdata, level, buf):
-    print("log: ",buf)
+    #print("log: ",buf)
 
 def wait_for(client,msgType,period=0.25):
     if msgType=="SUBACK":
@@ -35,16 +46,16 @@ def wait_for(client,msgType,period=0.25):
                 client.loop()  #check for messages
                 time.sleep(period)
 
-def publish_back(topic, payload, qos):
+def publish_back(topic, payload, qos, mid):
     topic=topic+"_2"
-    print("publish message Hello World! on topic test, qos_level="+str(qos_level))
     pub_rc = client.publish(topic,payload, qos_level, False)
-    print("publish returned ",pub_rc)
+    logger.info("message sent - id:"+mid+" - topic:"+topic+" - qos:"+qos_level+" - size:"+str(len(payload)))
 
 broker_address="192.168.1.100"
 port=1883
 qos_level=0
 client_name="Stolz"
+topic="test"
 
 print("creating new instance "+client_name)
 client = mqtt.Client(client_name) #create new client instance
@@ -56,18 +67,14 @@ client.on_connect=on_connect
 client.on_disconnect=on_disconnect
 
 # Connection to Broker
-print("connecting to broker "+broker_address+":"+str(port))
+logger.info("connecting to broker "+broker_address+":"+str(port))
 client.connect(broker_address,port) #connect to broker
 client.loop_start() #start the loop
 
-#while not client.connected_flag:
-#    print("waiting for connect")
-#    time.sleep(2)
- 
 
-print("Subscribing to topic","test")
+logger.info("Subscribing to topic",topic)
 sub_rc = client.subscribe("test",qos_level)
-print("subscribe returned ",sub_rc)
+logger.info("subscribe returned ",sub_rc)
 wait_for(client, sub_rc)
 
 while ( 1 ):
